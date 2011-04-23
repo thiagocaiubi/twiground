48c1dfb911b8985e10494782544c3eef46861f70
Thiago Caiubi <thiago.caiubi@gmail.com>
2011-04-22 22:14:20 -0300

    Initial commit: renders the background image into a simple div

---
+++ twiground.js
@@ -0,0 +1,46 @@
+var Twiground = {
+	url: function(text){
+		var e=/((http|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+\.[^\"#?\s]+)(#[\w\-]+)?/;
+		if (text.match(e)) {
+			return  {
+				url: RegExp['$&'],
+                		protocol: RegExp.$2,
+                		host:RegExp.$3,
+                		path:RegExp.$4,
+                		file:RegExp.$6,
+                		hash:RegExp.$7
+			}['url'];
+		}
+	},
+	image: function(){
+        var host = document.location.host;
+
+        if (host != "twitter.com") {
+            window.alert("Ops, looks like you aren't at twitter.com");
+            new Error(":(");
+        }
+
+        var img = $("body").css('backgroundImage');
+        return this.url(img);
+	},
+    facebox: function(){
+        var link = document.createElement("link");
+        link.media = "screen";
+        link.type = "text/css";
+        link.rel = "stylesheet"
+        link.href = "https://github.com/thiagocaiubi/facebox/raw/master/src/facebox.css";
+        $(link).appendTo(document.head);
+        console.log(link.readyStateChange);
+        
+        var script = document.createElement("script");
+        script.src = "https://github.com/thiagocaiubi/facebox/raw/master/src/facebox.js";
+        $(script).appendTo(document.body);
+        
+    },
+    reveal: function(){
+        //this.facebox();
+        var image = $("<img src=\""+ this.image() +"\"/>");
+        $("<div style=\"position:absolute; left:0;top:0;z-index:99999;\"></div>").html(image).appendTo(document.body);
+    }
+}
+Twiground.reveal();
