b88e9aed3d1599a8c7999d43dde4ecf2a1592538
Thiago Caiubi <thiago.caiubi@gmail.com>
2011-04-23 12:52:56 -0300

    Much simple and better: slide up and down the timeline

--- twiground.js
+++ twiground.js
@@ -1,43 +1,27 @@
 var Twiground = {
-    ratio: .9,
-	url: function(text){
-		var e=/((http|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+\.[^\"#?\s]+)(#[\w\-]+)?/;
-		if (text.match(e)) {
-			return  {
-				url: RegExp['$&'],
-                		protocol: RegExp.$2,
-                		host:RegExp.$3,
-                		path:RegExp.$4,
-                		file:RegExp.$6,
-                		hash:RegExp.$7
-			}['url'];
-		}
-	},
-	image: function(){
+	checkTwitter: function(){
         var host = document.location.host;
 
         if (host != "twitter.com") {
             window.alert("Ops, looks like you aren't at twitter.com");
             new Error(":(");
         }
-
-        var img = $("body").css('backgroundImage');
-        return this.url(img);
 	},
+    createGlobalNavLink: function(){
+        $("<li><a id=\"global-nav-twiground\" href=\"#\">Timeline</a></li>").appendTo("#global-nav ul");
+    },
+    toggleBackground: function(){
+        var twitground = $("#global-nav-twiground"),
+            pageOuter = $("#page-outer").slideUp("slow", function(){
+                twitground.click(function(){
+                    pageOuter.slideDown("slow");
+                });
+            });
+    },
     reveal: function(){
-        var image = $("<img src=\""+ this.image() +"\"/>");
-        $("<div />")
-            .css({
-                position: "absolute", 
-                left: "0",
-                top: "0",
-                zIndex: "99999"
-            })  
-            .html(image)
-            .click(function(){
-                alert(this);
-            })
-            .appendTo(document.body);
+        this.checkTwitter();
+        this.createGlobalNavLink();
+        this.toggleBackground();
     }
-}
+};
 Twiground.reveal();
